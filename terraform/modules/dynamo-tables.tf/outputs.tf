c4b825e8a096e83b9b4e7359ed7db450491a685e
GabelBombe <dodomeki@gmail.com>
2019-08-09 13:05:47 -0700

    Dynamo tables ready

---
+++ terraform/modules/dynamo-tables.tf/outputs.tf
@@ -0,0 +1,11 @@
+output "all_dynamodb_arns_and_indeces_for_access_rights" {
+  value = [
+    "${module.dynamodb-session.dynamodb_arn}",
+    "${module.dynamodb-users.dynamodb_arn}",
+    "${module.dynamodb-product-group.dynamodb_arn}",
+    "${aws_dynamodb_table.product-dynamodb-instance.arn}",
+
+    # NOTE: You have to give access right to the indices as well.
+    "${aws_dynamodb_table.product-dynamodb-instance.arn}/index/PGIndex",
+  ]
+}
